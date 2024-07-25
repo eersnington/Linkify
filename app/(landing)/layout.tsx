0219a9b9fdf998386e356005267a42240309d5c0
Sree Narayanan <sreeaadhi07@gmail.com>
2024-07-25 07:19:15 +0400

    Update layout.tsx

--- app/(landing)/layout.tsx
+++ app/(landing)/layout.tsx
@@ -4,6 +4,7 @@ import { Analytics } from "@vercel/analytics/react";
 import { marketingConfig } from "@/config/marketing";
 import { NavBar } from "@/components/layout/navbar";
 import { SiteFooter } from "@/components/layout/site-footer";
+import { LandingNavBar } from "@/components/layout/landing-nav";
 
 interface MarketingLayoutProps {
   children: React.ReactNode;
@@ -14,9 +15,8 @@ export default async function MarketingLayout({
 }: MarketingLayoutProps) {
   return (
     <div className="flex min-h-screen flex-col">
-      <Analytics />
       <Suspense fallback="...">
-        <NavBar items={marketingConfig.mainNav} scroll={true} />
+        <LandingNavBar items={marketingConfig.mainNav}  scroll={true}/>
       </Suspense>
       <main className="flex-1">{children}</main>
       <SiteFooter />
