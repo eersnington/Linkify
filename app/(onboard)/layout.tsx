778b93f15c2ff20c42ae40730c1bc1c18be6307a
Sree Narayanan <sreeaadhi07@gmail.com>
2024-07-25 10:01:37 +0400

    Update layout.tsx

--- app/(onboard)/layout.tsx
+++ app/(onboard)/layout.tsx
@@ -9,14 +9,14 @@ interface LayoutProps {
 
 export default function OnboardLayout({ children }: LayoutProps) {
   return (
-    <div className="flex min-h-screen flex-col bg-gradient-to-r from-indigo-700 via-indigo-600 to-indigo-700">
+    <div className="flex min-h-screen flex-col bg-purple-950">
       <Suspense fallback="...">
         <header className="flex items-center p-4">
           <Link
             href="/"
             className="flex items-center text-lg font-bold text-white"
           >
-            <Logo className="mr-2 text-white" />
+            <Logo className="mr-2 rounded-lg" />
             Linkify
           </Link>
         </header>
