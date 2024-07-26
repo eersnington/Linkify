7b1bbb8b487c24332eccbb77bf0f52b2b8c69f59
Sree Narayanan <sreeaadhi07@gmail.com>
2024-07-26 07:55:00 +0400

    Update layout.tsx

--- app/(onboard)/onboard/mypage/layout.tsx
+++ app/(onboard)/onboard/mypage/layout.tsx
@@ -1,4 +1,5 @@
-import { ThemeTemplateProvider } from "@/context/editor-sidebar-context";
+import { ThemeTemplateProvider } from '@/context/editor-sidebar-context';
+import { LinkedInDataProvider } from '@/context/linkedin-data-context';
 
 interface DashboardLayoutProps {
   children?: React.ReactNode;
@@ -8,7 +9,9 @@ export default async function PageLayout({ children }: DashboardLayoutProps) {
   return (
     <div className="flex h-screen flex-col">
       <div className="flex-1 overflow-hidden">
-        <ThemeTemplateProvider>{children}</ThemeTemplateProvider>
+        <ThemeTemplateProvider>
+          <LinkedInDataProvider>{children}</LinkedInDataProvider>
+        </ThemeTemplateProvider>
       </div>
     </div>
   );
