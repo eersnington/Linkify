afbd34075702aa33e37c6831105c36bf7d137e06
Sree Narayanan <sreeaadhi07@gmail.com>
2024-07-26 15:30:14 +0400

    Update navbar.tsx

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -1,17 +1,16 @@
-"use client";
+'use client';
 
-import Link from "next/link";
-import { MainNavItem } from "@/types";
-import { useUser } from "@clerk/nextjs";
+import Link from 'next/link';
+import { MainNavItem } from '@/types';
+import { useUser } from '@clerk/nextjs';
 
-import { cn } from "@/lib/utils";
-import useScroll from "@/hooks/use-scroll";
-import { useSigninModal } from "@/hooks/use-signin-modal";
-import { Button, buttonVariants } from "@/components/ui/button";
+import { cn } from '@/lib/utils';
+import useScroll from '@/hooks/use-scroll';
+import { Button, buttonVariants } from '@/components/ui/button';
 
-import { Icons } from "../shared/icons";
-import { MainNav } from "./main-nav";
-import { UserAccountNav } from "./user-account-nav";
+import { Icons } from '../shared/icons';
+import { MainNav } from './main-nav';
+import { UserAccountNav } from './user-account-nav';
 
 interface NavBarProps {
   items?: MainNavItem[];
@@ -27,18 +26,17 @@ export function NavBar({
   scroll = false,
 }: NavBarProps) {
   const scrolled = useScroll(50);
-  const signInModal = useSigninModal();
   const { isLoaded, isSignedIn, user } = useUser();
 
   const userObj = {
-    name: user?.fullName || "",
-    email: user?.emailAddresses[0]?.emailAddress || "",
+    name: user?.fullName || '',
+    email: user?.emailAddresses[0]?.emailAddress || '',
   };
 
   return (
     <header
       className={`sticky top-0 z-40 flex w-full justify-center bg-background/60 backdrop-blur-xl transition-all ${
-        scroll ? (scrolled ? "border-b" : "bg-background/0") : "border-b"
+        scroll ? (scrolled ? 'border-b' : 'bg-background/0') : 'border-b'
       }`}
     >
       <div className="container flex h-[60px] items-center justify-between py-4">
@@ -55,12 +53,12 @@ export function NavBar({
               // className="flex gap-2 bg-transparent px-4 text-sm font-semibold hover:bg-transparent hover:text-purple-600"
               className={cn(
                 buttonVariants({
-                  variant: "default",
-                  size: "sm",
-                  rounded: "lg",
+                  variant: 'default',
+                  size: 'sm',
+                  rounded: 'lg',
                 }),
-                "bg-slate-800 text-sm font-semibold hover:bg-purple-600",
-                "px-4",
+                'bg-slate-800 text-sm font-semibold hover:bg-purple-600',
+                'px-4'
               )}
             >
               <span>Sign In</span>
