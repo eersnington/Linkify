6bc1f05945d84641538091e6ce766b19e18c4f55
Sree Narayanan <sreeaadhi07@gmail.com>
2024-07-26 15:30:27 +0400

    Update loading.tsx

--- app/(onboard)/onboard/mypage/loading.tsx
+++ app/(onboard)/onboard/mypage/loading.tsx
@@ -1,12 +1,15 @@
-import { Button } from "@/components/ui/button";
-import { DashboardHeader } from "@/components/dashboard/header";
-import { DashboardShell } from "@/components/dashboard/shell";
-import { CardSkeleton } from "@/components/shared/card-skeleton";
+import { Button } from '@/components/ui/button';
+import { DashboardHeader } from '@/components/dashboard/header';
+import { DashboardShell } from '@/components/dashboard/shell';
+import { CardSkeleton } from '@/components/shared/card-skeleton';
 
 export default function DashboardBillingLoading() {
   return (
     <DashboardShell>
-      <DashboardHeader heading="Page" text=" Customize your webpage here">
+      <DashboardHeader
+        heading="Editor"
+        text="Customise your page and change templates any time"
+      >
         <Button>Publish</Button>
       </DashboardHeader>
       <div className="grid gap-10">
