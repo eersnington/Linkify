880cbfc966ca176d583f3b82879fe8469c617891
Sree Narayanan <sreeaadhi07@gmail.com>
2024-07-26 15:30:31 +0400

    Update page.tsx

--- app/(onboard)/onboard/error/page.tsx
+++ app/(onboard)/onboard/error/page.tsx
@@ -1,16 +1,24 @@
-import { Metadata } from "next";
+import { Metadata } from 'next';
 import { currentUser } from '@clerk/nextjs/server';
+import { LinkedInLogoIcon } from '@radix-ui/react-icons';
 
-import { cn } from "@/lib/utils";
-import { pf_display } from "@/app/fonts";
-import { redirect } from "next/navigation";
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
-import { Label } from "@/components/ui/label";
-import Link from "next/link";
+import { cn } from '@/lib/utils';
+import { pf_display } from '@/app/fonts';
+import { redirect } from 'next/navigation';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import { Label } from '@/components/ui/label';
+import Link from 'next/link';
 
 export const metadata: Metadata = {
-  title: "Onboard",
-  description: "Onboarding of Linkify",
+  title: 'Onboard',
+  description: 'Onboarding of Linkify',
 };
 
 export default async function OnboardPage({
@@ -22,54 +30,46 @@ export default async function OnboardPage({
 }) {
   const { email } = searchParams;
 
-  const user = await currentUser(); 
+  const user = await currentUser();
 
-    if (user){
-        redirect("/dashboard");
-    }
+  if (user) {
+    redirect('/dashboard');
+  }
+
+  if (email === 'undefined' || !email) {
+    redirect('/');
+  }
 
   return (
     <div className="flex flex-col items-center justify-center space-y-12 px-4 text-center">
-      <div className="mx-auto max-w-2xl">
-        <div
-          className={cn(
-            "text-5xl font-bold leading-snug text-slate-200",
-            pf_display.className,
-          )}
-        >
-          {"“I setup a portfolio in 5 minutes with Linkify."}
-          <br />
-          {"It was so easy!” "}
-          <span className="mt-4 font-mono text-xl font-bold text-slate-900">
-            - Our users
-          </span>
-        </div>
-      </div>
       <div className="w-full max-w-md">
-        <Card className="mx-auto w-full max-w-md border-2 border-slate-900 drop-shadow-xl">
-      <CardHeader>
-        <CardTitle>Error</CardTitle>
-        <CardDescription>
-          Please verify the following information to continue.
-        </CardDescription>
-      </CardHeader>
-      <CardContent>
-        <div className="flex flex-col items-center justify-center space-y-4 ">
-                   <div className="space-y-2">
-            <Label className="text-lg">We couldn&apos;t find a LinkedIn account associated to {" "}{email}</Label>
-          </div>
-        </div>
-      </CardContent>
-      <CardFooter>
-        <div className="flex w-full justify-center gap-14">
-          <Link
-          href={"/"}
-          className="bg-black rounded-lg text-base hover:bg-black/80 text-white px-4 py-2 font-bold">
-            {"Try Again?"}
-          </Link>
-        </div>
-      </CardFooter>
-    </Card>
+        <Card className="mx-auto w-full max-w-md border-2 border-purple-700 drop-shadow-xl">
+          <CardHeader className="text-purple-950">
+            <CardTitle className="flex items-center justify-center space-x-2">
+              <LinkedInLogoIcon className="text-purple-700" />
+              Error
+            </CardTitle>
+          </CardHeader>
+          <CardContent>
+            <div className="flex flex-col items-center justify-center space-y-4 ">
+              <div className="space-y-2">
+                <Label className="text-lg  text-purple-950">
+                  We couldn&apos;t find a LinkedIn account associated to {email}
+                </Label>
+              </div>
+            </div>
+          </CardContent>
+          <CardFooter>
+            <div className="flex w-full justify-center gap-14">
+              <Link
+                href={'/'}
+                className="bg-yellow-500 rounded-lg text-base hover:bg-purple-700 text-white px-4 py-2 font-bold"
+              >
+                {'Try Again?'}
+              </Link>
+            </div>
+          </CardFooter>
+        </Card>
       </div>
     </div>
   );
