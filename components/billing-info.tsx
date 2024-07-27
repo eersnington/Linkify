96787ebd6fae839f81f6650a8ed42fc3f8c669ae
Sree Narayanan <sreeaadhi07@gmail.com>
2024-07-27 11:46:29 +0400

    Update billing-info.tsx

--- components/billing-info.tsx
+++ components/billing-info.tsx
@@ -48,7 +48,13 @@ export function BillingInfo({ userSubscriptionPlan }: BillingInfoProps) {
       <CardHeader>
         <CardTitle className="text-xl">Subscription Plan</CardTitle>
         <CardDescription className="text-base">
-          You are currently on the <strong>{title}</strong> plan.
+          You are currently on the{' '}
+          {isPaid ? (
+            <strong className="text-purple-500">{title}</strong>
+          ) : (
+            <strong className="text-gray-500">{title}</strong>
+          )}{' '}
+          plan.
         </CardDescription>
       </CardHeader>
       <CardContent>{description}</CardContent>
@@ -56,66 +62,14 @@ export function BillingInfo({ userSubscriptionPlan }: BillingInfoProps) {
         {isPaid && stripeCustomerId ? (
           <CustomerPortalButton userStripeId={stripeCustomerId} />
         ) : (
-          <Dialog>
-            <DialogTrigger asChild>
-              <Button className={cn(buttonVariants())}>Choose a plan</Button>
-            </DialogTrigger>
-            <DialogContent className="bg-slate-50 sm:max-w-[425px]">
-              <DialogHeader>
-                <DialogTitle className="text-gradient_indigo-purple flex items-center gap-2 text-2xl font-bold">
-                  <Sparkles className="size-6 text-yellow-500" />
-                  Unlock Premium Theme
-                </DialogTitle>
-                <DialogDescription className="text-lg">
-                  Elevate your profile with our exclusive premium theme!
-                </DialogDescription>
-              </DialogHeader>
-              <div className="grid gap-4 py-4">
-                <div className="flex items-center gap-4">
-                  <div className="rounded-full bg-purple-100 p-2">
-                    <Sparkles className="size-6 text-purple-600" />
-                  </div>
-                  <div className="text-sm">
-                    <p className="font-semibold">Enhanced Visual Appeal</p>
-                    <p className="text-gray-500">
-                      Stand out with unique designs
-                    </p>
-                  </div>
-                </div>
-                <div className="flex items-center gap-4">
-                  <div className="rounded-full bg-purple-100 p-2">
-                    <Crown className="size-6 text-purple-600" />
-                  </div>
-                  <div className="text-sm">
-                    <p className="font-semibold">Professional Edge</p>
-                    <p className="text-gray-500">Impress potential employers</p>
-                  </div>
-                </div>
-                <div className="flex items-center gap-4">
-                  <div className="rounded-full bg-purple-100 p-2">
-                    <Zap className="size-6 text-purple-600" />
-                  </div>
-                  <div className="text-sm">
-                    <p className="font-semibold">Advanced Features</p>
-                    <p className="text-gray-500">
-                      Custom domain, web analytics, and AI content
-                    </p>
-                  </div>
-                </div>
-              </div>
-              <DialogFooter>
-                <Button
-                  type="submit"
-                  className="bg-purple-600 hover:bg-purple-700"
-                  onClick={() => router.push('/pricing')}
-                >
-                  Subscribe Now
-                </Button>
-              </DialogFooter>
-            </DialogContent>
-          </Dialog>
+          <Button
+            type="submit"
+            className="bg-purple-600 hover:bg-purple-700"
+            onClick={() => router.push('/dashboard/checkout')}
+          >
+            Subscribe Now
+          </Button>
         )}
-
         {isPaid ? (
           <p className="rounded-full text-xs font-medium">
             {isCanceled
