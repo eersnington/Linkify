c8eb4f55a776f763bdcc400955b39df8cfa9ee3a
Sree Narayanan <sreeaadhi07@gmail.com>
2024-07-27 21:39:17 +0400

    Update loading.tsx

--- app/(dashboard)/dashboard/loading.tsx
+++ app/(dashboard)/dashboard/loading.tsx
@@ -1,17 +1,17 @@
-import { CardSkeleton } from "@/components/shared/card-skeleton"
-import { DashboardHeader } from "@/components/dashboard/header"
-import { DashboardShell } from "@/components/dashboard/shell"
-import { Button } from "@/components/ui/button"
+import { CardSkeleton } from '@/components/shared/card-skeleton';
+import { DashboardHeader } from '@/components/dashboard/header';
+import { DashboardShell } from '@/components/dashboard/shell';
 
 export default function DashboardLoading() {
   return (
     <DashboardShell>
-      <DashboardHeader heading="Posts" text="Create and manage posts.">
-        <Button>Fake button</Button>
-      </DashboardHeader>
+      <DashboardHeader
+        heading="Dashboard"
+        text="View your Web Analytics here"
+      />
       <div className="divide-border-200 divide-y rounded-md border">
         <CardSkeleton />
       </div>
     </DashboardShell>
-  )
+  );
 }
