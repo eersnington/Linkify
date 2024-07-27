eb4a663295538f473befa29d32326cba9644074a
Sree Narayanan <sreeaadhi07@gmail.com>
2024-07-27 21:39:49 +0400

    Update page.tsx

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,11 +1,12 @@
-import { FeatureSection } from "@/components/sections/components/features";
-import { HeroLanding } from "@/components/sections/hero-landing";
+import { FeatureSection } from '@/components/sections/components/features';
+import { HeroLanding } from '@/components/sections/hero-landing';
+import { ThreeSteps } from '@/components/sections/three-steps';
 
 export default async function IndexPage() {
   return (
     <>
       <HeroLanding />
-      <FeatureSection />
+      <ThreeSteps />
     </>
   );
 }
