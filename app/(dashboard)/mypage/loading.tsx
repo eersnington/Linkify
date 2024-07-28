f77f22d36db03b9f5ee0410ad6333e6b1f7a87c8
Sree Narayanan <sreeaadhi07@gmail.com>
2024-07-28 07:32:11 +0400

    Update loading.tsx

--- app/(dashboard)/mypage/loading.tsx
+++ app/(dashboard)/mypage/loading.tsx
@@ -1,17 +1,39 @@
-import { Button } from "@/components/ui/button";
-import { DashboardHeader } from "@/components/dashboard/header";
-import { DashboardShell } from "@/components/dashboard/shell";
-import { CardSkeleton } from "@/components/shared/card-skeleton";
+import { cn } from '@/lib/utils';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import { Skeleton } from '@/components/ui/skeleton';
+import { LoaderIcon } from 'lucide-react';
+import { LinkedInLogoIcon } from '@radix-ui/react-icons';
 
-export default function DashboardBillingLoading() {
+export default function Loading() {
   return (
-    <DashboardShell>
-      <DashboardHeader heading="Page" text=" Customize your webpage here">
-        <Button>Publish</Button>
-      </DashboardHeader>
-      <div className="grid gap-10">
-        <CardSkeleton />
+    <div className="flex flex-col items-center justify-center space-y-12 px-4 text-center">
+      <div className="w-full max-w-md">
+        <Card className="mx-auto w-full max-w-md border-2 border-purple-700 drop-shadow-xl">
+          <CardHeader className="text-purple-950">
+            <CardTitle className="flex items-center justify-center space-x-2">
+              <LinkedInLogoIcon className="animate-bounce text-purple-700" />
+              <span>User Information</span>
+            </CardTitle>
+            <CardDescription>
+              We&apos;re fetching your LinkedIn profile. Please wait...
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <div className="mx-auto my-4 flex w-full flex-col items-center gap-5">
+              <LoaderIcon className="animate-spin text-purple-700" size={48} />
+              <Skeleton className="mb-2 h-4 w-1/12 bg-yellow-500" />
+              <Skeleton className="h-10 w-2/6 bg-purple-950" />
+              <Skeleton className="mb-3 mt-5 h-8 w-1/5 bg-yellow-500" />
+            </div>
+          </CardContent>
+        </Card>
       </div>
-    </DashboardShell>
+    </div>
   );
 }
