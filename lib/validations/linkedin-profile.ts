7f8c2d0ecee7f63bbe79a445828c431c01914e93
Sree Narayanan <sreeaadhi07@gmail.com>
2024-07-28 20:12:47 +0400

    Update linkedin-profile.ts

--- lib/validations/linkedin-profile.ts
+++ lib/validations/linkedin-profile.ts
@@ -1,26 +1,60 @@
 // lib/validation/linkedin-profile.ts
-import { z } from "zod";
+import { z } from 'zod';
 
-const workExperienceSchema = z.object({
-  title: z.string().min(1, "Title is required"),
-  company: z.string().min(1, "Company name is required"),
-  date: z.string().min(1, "Date is required"),
-  description: z.string().min(1, "Description is required"),
+const workExperienceFormSchema = z.object({
+  title: z.string().min(1, 'Title is required'),
+  company: z.string().min(1, 'Company name is required'),
+  date: z.string().min(1, 'Date is required'),
+  description: z.string().min(1, 'Description is required'),
 });
 
-const educationSchema = z.object({
-  name: z.string().min(1, "School name is required"),
-  degree: z.string().min(1, "Degree name is required"),
-  date: z.string().min(1, "Date is required"),
+const educationFormSchema = z.object({
+  name: z.string().min(1, 'School name is required'),
+  degree: z.string().min(1, 'Degree name is required'),
+  date: z.string().min(1, 'Date is required'),
 });
 
-export const linkedInProfileSchema = z.object({
-  fullName: z.string().min(1, "Full name is required"),
-  title: z.string().min(1, "Title is required"),
-  description: z.string().min(1, "Description is required"),
-  photoUrl: z.string().url("Invalid URL"),
-  workExperiences: z.array(workExperienceSchema),
-  education: z.array(educationSchema),
+export const linkedInProfileFormSchema = z.object({
+  firstName: z.string().min(1, 'First name is required'),
+  lastName: z.string().min(1, 'Last name is required'),
+  title: z.string().min(1, 'Title is required'),
+  description: z.string().min(1, 'Description is required'),
+  photoUrl: z.string().url('Invalid URL'),
+  workExperiences: z.array(workExperienceFormSchema),
+  education: z.array(educationFormSchema),
 });
 
-export type LinkedInProfileFormData = z.infer<typeof linkedInProfileSchema>;
+export const AILinkedInProfileSchema = z.object({
+  firstName: z.string(),
+  lastName: z.string(),
+  photoUrl: z.string(),
+  title: z.string(),
+  description: z.string(),
+  linkedInUrl: z.string(),
+  certifications: z.array(
+    z.object({
+      name: z.string(),
+      organization: z.string(),
+      date: z.string(),
+    })
+  ),
+  workExperiences: z.array(
+    z.object({
+      title: z.string(),
+      company: z.string(),
+      date: z.string(),
+      description: z.string(),
+    })
+  ),
+  education: z.array(
+    z.object({
+      name: z.string(),
+      degree: z.string(),
+      date: z.string(),
+    })
+  ),
+  skills: z.array(z.string()),
+});
+
+export type LinkedInProfileFormData = z.infer<typeof linkedInProfileFormSchema>;
+export type AILinkedInProfileFormData = z.infer<typeof AILinkedInProfileSchema>;
