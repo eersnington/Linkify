f769f611352e5a08484e91fcf52ba77b30752be7
Sree Narayanan <sreeaadhi07@gmail.com>
2024-07-30 10:38:34 +0400

    Update analytics.tsx

--- components/analytics.tsx
+++ components/analytics.tsx
@@ -5,7 +5,7 @@ import axios from 'axios';
 
 export default function AnalyticsTracker() {
   const [userCountry, setUserCountry] = useState('Unknown');
-
+ 
   useEffect(() => {
     const fetchGeoData = async () => {
       try {
