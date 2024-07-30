74f2d27c9dbe62de699e1f615c6b5c920fd5bbd3
Sree Narayanan <sreeaadhi07@gmail.com>
2024-07-30 14:26:59 +0400

    Update page.tsx

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -14,11 +14,15 @@ import {
 import { Button } from '@/components/ui/button';
 import { DashboardHeader } from '@/components/dashboard/header';
 import { DashboardShell } from '@/components/dashboard/shell';
-import { PlusSquare } from 'lucide-react';
+import { ExternalLink, PlusSquare } from 'lucide-react';
+import { Badge } from '@/components/ui/badge';
+import { env } from '@/env.mjs';
 
 export const metadata = {
   title: 'Dashboard',
 };
+
+const URL = env.NEXT_PUBLIC_URL;
 
 export default async function DashboardPage() {
   const user = await currentUser();
@@ -31,6 +35,8 @@ export default async function DashboardPage() {
     where: { userEmail: user?.emailAddresses[0].emailAddress },
   });
 
+  const domainName = website?.domainName;
+
   return (
     <DashboardShell className="">
       <DashboardHeader heading="Dashboard" text="View Your Web Analytics" />
@@ -38,7 +44,19 @@ export default async function DashboardPage() {
         {website ? (
           <Card className="shadow-md rounded-md">
             <CardHeader>
-              <CardTitle>Website Performance</CardTitle>
+              <CardTitle>
+                Website Performance{' '}
+                <Badge className="text-center items-center">
+                  <Link
+                    href={`${URL}/${domainName}`}
+                    target="_blank"
+                    className="text-white hover:underline flex items-center justify-center mt-2"
+                  >
+                    {URL}/{domainName}{' '}
+                    <ExternalLink className="ml-2" size={12} />
+                  </Link>
+                </Badge>
+              </CardTitle>
               <CardDescription>
                 Overview of your website&apos;s traffic
               </CardDescription>
