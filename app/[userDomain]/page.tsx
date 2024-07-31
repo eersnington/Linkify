7fa3c7a2284e063cfd2d87e1978374d2edf6cebe
Sree Narayanan <sreeaadhi07@gmail.com>
2024-07-31 19:08:11 +0400

    updates to middle ware for multinenant app
    Multi tenant project structure
    
    still bugs with clerk on dashboard
    
    no idea if it is the dashboard

--- app/[userDomain]/page.tsx
+++ app/[userDomain]/page.tsx
@@ -1,6 +1,7 @@
-// app/[userDomain]/page.tsx
+// [userDomain] is a dynamic route that will be used to render the user's page
 
 import { notFound } from 'next/navigation';
+import { headers } from 'next/headers';
 
 import UserPageContent from './_component/user-page-content';
 import { prisma } from '@/lib/db';
@@ -8,16 +9,47 @@ import { ThemeTemplateProvider } from '@/context/editor-sidebar-context';
 import AnalyticsTracker from '@/components/analytics';
 import NotFoundScreen from '@/components/notfound';
 
-export async function generateMetadata({
-  params,
-}: {
-  params: { userDomain: string };
-}) {
-  const website = await prisma.website.findUnique({
-    where: { domainName: params.userDomain },
-  });
+const getSubdomain = (host: string, rootDomain: string) => {
+  // Check if it's a localhost domain
+  if (host.includes('localhost')) {
+    const parts = host.split('.');
+    if (parts.length > 1) {
+      return parts[0];
+    }
+    return null;
+  }
 
-  console.log(website);
+  // Check for public domain
+  if (host.endsWith(rootDomain)) {
+    const parts = host.split('.');
+    // Check if there's a subdomain
+    if (parts.length > 2) {
+      return parts[0];
+    }
+  }
+
+  return null;
+};
+
+export async function generateMetadata() {
+  const headersList = headers();
+  const hostname = headersList.get('host') || '';
+  const rootDomain = process.env.NEXT_PUBLIC_ROOT_DOMAIN;
+
+  const subdomain = getSubdomain(hostname, rootDomain || '');
+
+  console.log('Hostname:', hostname);
+  console.log('Subdomain:', subdomain);
+
+  if (!subdomain) {
+    return {
+      title: 'Page Not Found',
+    };
+  }
+
+  const website = await prisma.website.findUnique({
+    where: { domainName: subdomain },
+  });
 
   if (!website) {
     return {
@@ -38,13 +70,20 @@ export async function generateMetadata({
   };
 }
 
-export default async function UserPage({
-  params,
-}: {
-  params: { userDomain: string };
-}) {
+export default async function UserPage() {
+  const headersList = headers();
+  const hostname = headersList.get('host') || '';
+  const rootDomain = process.env.NEXT_PUBLIC_ROOT_DOMAIN;
+
+  const subdomain = getSubdomain(hostname, rootDomain || '');
+
+  if (!subdomain) {
+    console.log('Subdomain not found');
+    return <NotFoundScreen />;
+  }
+
   const website = await prisma.website.findUnique({
-    where: { domainName: params.userDomain },
+    where: { domainName: subdomain },
   });
 
   if (!website) {
--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,48 @@
 import { clerkMiddleware } from '@clerk/nextjs/server';
+import { NextResponse } from 'next/server';
 
 export default clerkMiddleware((auth, req) => {
-  let hostname = req.headers.get('host');
+  const url = req.nextUrl;
+  console.log('url', url);
+
+  // Get hostname of request (e.g. sree.linkifyme.pro, sree.localhost:3000)
+  let hostname = req.headers
+    .get('host')!
+    .replace('.localhost:3000', `.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`);
 
   console.log('hostname', hostname);
+
+  // special case for Vercel preview deployment URLs
+  if (
+    hostname.includes('---') &&
+    hostname.endsWith(`.${process.env.NEXT_PUBLIC_VERCEL_DEPLOYMENT_SUFFIX}`)
+  ) {
+    hostname = `${hostname.split('---')[0]}.${
+      process.env.NEXT_PUBLIC_ROOT_DOMAIN
+    }`;
+  }
+
+  const searchParams = req.nextUrl.searchParams.toString();
+  // Get the pathname of the request (e.g. /, /about, /blog/first-post)
+  const path = `${url.pathname}${
+    searchParams.length > 0 ? `?${searchParams}` : ''
+  }`;
+
+  console.log('Path: ', path);
+
+  // rewrite root application to `/home` folder
+  if (
+    hostname === 'localhost:3000' ||
+    hostname === process.env.NEXT_PUBLIC_ROOT_DOMAIN
+  ) {
+    console.log('Rewriting to /home');
+    return NextResponse.rewrite(
+      new URL(`/home${path === '/' ? '' : path}`, req.url)
+    );
+  }
+
+  // rewrite everything else to `/[domain] dynamic route
+  return NextResponse.rewrite(new URL(`/${hostname}${path}`, req.url));
 });
 
 export const config = {
