1b814c476317b13caafde6421df5a6dd39ec892d
Sree Narayanan <sreeaadhi07@gmail.com>
2024-08-01 05:45:12 +0400

    env mjs update

--- env.mjs
+++ env.mjs
@@ -3,32 +3,57 @@ import { z } from 'zod';
 
 export const env = createEnv({
   server: {
-    // This is optional because it's only used in development.
-    // See https://next-auth.js.org/deployment.
-    NEXTAUTH_URL: z.string().url().optional(),
+    GROQ_API_KEY: z.string().min(1).optional(),
+    ANTHROPIC_API_KEY: z.string().min(1).optional(),
     DYNADOT_API_KEY: z.string().min(1),
     RAPID_API_KEY: z.string().min(1),
+    RESEND_API_KEY: z.string().min(1),
+
+    VERCEL_TOKEN: z.string().min(1),
+    VERCEL_PROJECT_ID: z.string().min(1),
+    VERCEL_TEAM_ID: z.string().min(1).optional(),
+
     DATABASE_URL: z.string().min(1),
     DATABASE_URL_NON_POOLING: z.string().min(1),
-    RESEND_API_KEY: z.string().min(1),
     STRIPE_API_KEY: z.string().min(1),
     STRIPE_WEBHOOK_SECRET: z.string().min(1),
+
+    CLERK_SECRET_KEY: z.string().min(1),
+    CLERK_WEBHOOK_SECRET: z.string().min(1),
   },
   client: {
     NEXT_PUBLIC_APP_URL: z.string().min(1),
     NEXT_PUBLIC_STRIPE_PRO_MONTHLY_PLAN_ID: z.string().min(1),
     NEXT_PUBLIC_STRIPE_PRO_YEARLY_PLAN_ID: z.string().min(1),
     NEXT_PUBLIC_ROOT_DOMAIN: z.string().min(1),
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string().min(1),
   },
   runtimeEnv: {
-    DYNADOT_API_KEY: process.env.DYNADOT_API_KEY,
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    // Vercel | App
+    VERCEL_TOKEN: process.env.VERCEL_TOKEN,
+    VERCEL_PROJECT_ID: process.env.VERCEL_PROJECT_ID,
+    VERCEL_TEAM_ID: process.env.VERCEL_TEAM_ID,
+    NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
     NEXT_PUBLIC_ROOT_DOMAIN: process.env.NEXT_PUBLIC_ROOT_DOMAIN,
+
+    // Database
     DATABASE_URL: process.env.DATABASE_URL,
     DATABASE_URL_NON_POOLING: process.env.DATABASE_URL_NON_POOLING,
+
+    // API keys
     RESEND_API_KEY: process.env.RESEND_API_KEY,
     RAPID_API_KEY: process.env.RAPID_API_KEY,
-    NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
+    DYNADOT_API_KEY: process.env.DYNADOT_API_KEY,
+    GROQ_API_KEY: process.env.GROQ_API_KEY,
+    ANTHROPIC_API_KEY: process.env.ANTHROPIC_API_KEY,
+
+    // Clerk Auth
+
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
+      process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+    CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+    CLERK_WEBHOOK_SECRET: process.env.CLERK_WEBHOOK_SECRET,
+
     // Stripe
     STRIPE_API_KEY: process.env.STRIPE_API_KEY,
     STRIPE_WEBHOOK_SECRET: process.env.STRIPE_WEBHOOK_SECRET,
