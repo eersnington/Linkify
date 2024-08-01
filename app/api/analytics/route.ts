2165006b9b124e2ecbd2b515183b61c3d356a797
Sree Narayanan <sreeaadhi07@gmail.com>
2024-08-01 05:32:15 +0400

    Update route.ts

--- app/api/analytics/route.ts
+++ app/api/analytics/route.ts
@@ -3,6 +3,7 @@ import { NextRequest, NextResponse } from 'next/server';
 import { prisma } from '@/lib/db';
 
 export async function POST(request: NextRequest) {
+  console.log('Analytics API called by ', request.headers.get('host'));
   const { page, referrer, userAgent, country } = await request.json();
   const userDomain = request.headers.get('host') || '';
   console.log('Recording analytics:', {
@@ -12,37 +13,37 @@ export async function POST(request: NextRequest) {
     country,
     userDomain,
   });
-  // try {
-  //   const cleanedPage = page.startsWith('/') ? page.substring(1) : page;
-  //   const website = await prisma.website.findUnique({
-  //     where: { domainName: cleanedPage },
-  //   });
-  //   if (!website) {
-  //     return NextResponse.json(
-  //       { message: 'Website not found' },
-  //       { status: 404 }
-  //     );
-  //   }
-  //   const pageView = await prisma.pageView.create({
-  //     data: {
-  //       page: cleanedPage,
-  //       referrer,
-  //       userAgent,
-  //       country,
-  //       timestamp: new Date(),
-  //       websiteId: website.id,
-  //     },
-  //   });
-  //   console.log('Analytics recorded:', pageView);
-  //   return NextResponse.json(
-  //     { message: 'Analytics recorded' },
-  //     { status: 200 }
-  //   );
-  // } catch (error) {
-  //   console.error('Error recording analytics:', error);
-  //   return NextResponse.json(
-  //     { message: 'Error recording analytics' },
-  //     { status: 500 }
-  //   );
-  // }
+  try {
+    const cleanedPage = page.startsWith('/') ? page.substring(1) : page;
+    const website = await prisma.website.findUnique({
+      where: { domainName: cleanedPage },
+    });
+    if (!website) {
+      return NextResponse.json(
+        { message: 'Website not found' },
+        { status: 404 }
+      );
+    }
+    const pageView = await prisma.pageView.create({
+      data: {
+        page: cleanedPage,
+        referrer,
+        userAgent,
+        country,
+        timestamp: new Date(),
+        websiteId: website.id,
+      },
+    });
+    console.log('Analytics recorded:', pageView);
+    return NextResponse.json(
+      { message: 'Analytics recorded' },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error('Error recording analytics:', error);
+    return NextResponse.json(
+      { message: 'Error recording analytics' },
+      { status: 500 }
+    );
+  }
 }
