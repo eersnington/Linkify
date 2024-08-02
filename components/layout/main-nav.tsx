027ba343c0a8f287e2d3620ac5dd7f85c0307238
Sree Narayanan <sreeaadhi07@gmail.com>
2024-08-02 12:04:37 +0400

    Update main-nav.tsx

--- components/layout/main-nav.tsx
+++ components/layout/main-nav.tsx
@@ -1,23 +1,24 @@
-"use client";
+'use client';
 
-import * as React from "react";
-import Link from "next/link";
-import { useSelectedLayoutSegment } from "next/navigation";
+import * as React from 'react';
+import Link from 'next/link';
+import { useSelectedLayoutSegment } from 'next/navigation';
 
-import { MainNavItem } from "types";
-import { siteConfig } from "@/config/site";
-import { cn } from "@/lib/utils";
-import { MobileNav } from "@/components/layout/mobile-nav";
-import { Icons } from "@/components/shared/icons";
+import { MainNavItem } from 'types';
+import { siteConfig } from '@/config/site';
+import { cn } from '@/lib/utils';
+import { MobileNav } from '@/components/layout/mobile-nav';
+import { Icons } from '@/components/shared/icons';
 
-import Logo from "../shared/logo";
+import Logo from '../shared/logo';
 
 interface MainNavProps {
   items?: MainNavItem[];
   children?: React.ReactNode;
+  isDashboard?: boolean;
 }
 
-export function MainNav({ items, children }: MainNavProps) {
+export function MainNav({ items, children, isDashboard }: MainNavProps) {
   const segment = useSelectedLayoutSegment();
   const [showMobileMenu, setShowMobileMenu] = React.useState<boolean>(false);
 
@@ -32,16 +33,19 @@ export function MainNav({ items, children }: MainNavProps) {
       }
     };
 
-    document.addEventListener("click", closeMobileMenuOnClickOutside);
+    document.addEventListener('click', closeMobileMenuOnClickOutside);
 
     return () => {
-      document.removeEventListener("click", closeMobileMenuOnClickOutside);
+      document.removeEventListener('click', closeMobileMenuOnClickOutside);
     };
   }, [showMobileMenu]);
 
   return (
     <div className="flex gap-6 md:gap-10">
-      <Link href="/" className="hidden items-center space-x-2 md:flex ">
+      <Link
+        href={isDashboard ? '/dashboard' : '/'}
+        className="hidden items-center space-x-2 md:flex "
+      >
         <Logo />
         <span className="hidden font-urban text-xl font-bold sm:inline-block">
           {siteConfig.name}
@@ -52,14 +56,14 @@ export function MainNav({ items, children }: MainNavProps) {
           {items?.map((item, index) => (
             <Link
               key={index}
-              href={item.disabled ? "#" : item.href}
+              href={item.disabled ? '#' : item.href}
               prefetch={true}
               className={cn(
-                "flex items-center text-lg font-semibold transition-colors hover:text-purple-600 sm:text-sm",
+                'flex items-center text-lg font-semibold transition-colors hover:text-purple-600 sm:text-sm',
                 item.href.startsWith(`/${segment}`)
-                  ? "text-foreground"
-                  : "text-foreground/60",
-                item.disabled && "cursor-not-allowed opacity-80",
+                  ? 'text-foreground'
+                  : 'text-foreground/60',
+                item.disabled && 'cursor-not-allowed opacity-80'
               )}
             >
               {item.title}
