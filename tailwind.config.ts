181fc1095b3f39db3d0d5330cff47d5a47d3f13f
Sree Narayanan <sreeaadhi07@gmail.com>
2024-08-05 14:11:59 +0400

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -78,6 +78,10 @@ const config = {
         heading: ['var(--font-heading)', ...fontFamily.sans],
       },
       keyframes: {
+        'caret-blink': {
+          '0%,70%,100%': { opacity: '1' },
+          '20%,50%': { opacity: '0' },
+        },
         'accordion-down': {
           from: { height: '0' },
           to: { height: 'var(--radix-accordion-content-height)' },
@@ -138,6 +142,8 @@ const config = {
         },
       },
       animation: {
+        'caret-blink': 'caret-blink 1.25s ease-out infinite',
+
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
 
