9c2833c08fb497389e9b14fce4c77919dda1295b
Sree Narayanan <sreeaadhi07@gmail.com>
2024-08-05 21:03:25 +0400

    Update page.tsx

--- app/home/(dashboard)/dashboard/admin/page.tsx
+++ app/home/(dashboard)/dashboard/admin/page.tsx
@@ -3,13 +3,33 @@ import { prisma } from '@/lib/db';
 import { DashboardHeader } from '@/components/dashboard/header';
 import { DashboardShell } from '@/components/dashboard/shell';
 import { AdminDashboardContent } from '@/components/admin-dashboard';
+import { currentUser } from '@clerk/nextjs/server';
+import { redirect } from 'next/navigation';
+
+const super_admin = 'sreeaadhi07@gmail.com';
 
 export default async function AdminDashboardPage() {
+  const user = await currentUser();
+
+  if (!user) {
+    redirect('/signin');
+  }
+
+  const userEmail = user.emailAddresses[0].emailAddress;
+
   const users = await prisma.user.findMany({
     include: {
       website: true,
     },
   });
+
+  const adminUsers = await prisma.adminUser.findMany();
+
+  if (userEmail !== super_admin) {
+    if (!adminUsers.find((adminUser) => adminUser.userEmail === userEmail)) {
+      redirect('/dashboard');
+    }
+  }
 
   return (
     <DashboardShell>
@@ -17,7 +37,7 @@ export default async function AdminDashboardPage() {
         heading="Admin Dashboard"
         text="View all users and their details."
       />
-      <AdminDashboardContent users={users} />
+      <AdminDashboardContent users={users} adminUsers={adminUsers} />
     </DashboardShell>
   );
 }
