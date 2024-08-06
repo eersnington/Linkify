2d8cf2c1447087d3a72c5ca2360c49b178cf2fc4
Sree Narayanan <sreeaadhi07@gmail.com>
2024-08-06 15:38:20 +0400

    Update source-form-submission.ts

--- actions/source-form-submission.ts
+++ actions/source-form-submission.ts
@@ -3,18 +3,28 @@
 import { prisma } from '@/lib/db';
 import { revalidatePath } from 'next/cache';
 
-export async function onboardFormSubmit(source: string, userId: string | undefined) {
+export async function onboardFormSubmit(
+  source: string,
+  userId: string | undefined
+) {
   if (!userId) {
     throw new Error('User ID is required');
   }
 
+  console.log(userId);
+
   try {
-    await prisma.source.create({
-      data: {
-        userId: userId,
-        source: source,
+    await prisma.source.upsert({
+      where: { userId },
+      update: {
+        source,
+      },
+      create: {
+        source,
+        userId,
       },
     });
+    console.log('submitted');
     revalidatePath('/dashboard');
   } catch (error) {
     console.error('Error saving source data:', error);
