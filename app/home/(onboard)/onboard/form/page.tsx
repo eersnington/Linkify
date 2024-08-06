505599b5b070d1c6dc22dfe86394c1fe9bd4d7ec
Sree Narayanan <sreeaadhi07@gmail.com>
2024-08-06 15:38:41 +0400

    Update page.tsx

--- app/home/(onboard)/onboard/form/page.tsx
+++ app/home/(onboard)/onboard/form/page.tsx
@@ -1,14 +1,27 @@
 import { OnboardingSourceForm } from '@/components/forms/onboard-question';
 import Logo from '@/components/shared/logo';
+import { prisma } from '@/lib/db';
 import { currentUser } from '@clerk/nextjs/server';
 import { redirect } from 'next/navigation';
 
 export default async function OnboardingPage() {
   const user = await currentUser();
 
-  if (user) {
+  if (!user) {
     redirect('/dashboard/');
   }
+
+  const userDb = await prisma.user.findFirst({
+    where: { id: user.id },
+    include: {
+      source: true,
+    },
+  });
+
+  if (userDb?.source) {
+    redirect('/dashboard/');
+  }
+
   return (
     <div className="min-h-screen bg-white flex items-center justify-center p-4">
       <div className="w-full max-w-2xl bg-white border-2 border-purple-500 rounded-xl shadow-xl p-8">
